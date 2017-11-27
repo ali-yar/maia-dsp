b66a266a017f270ff6e07776b54e5224dac65a7b
ali-yar <ali_berrada@etu.u-bourgogne.fr>
2017-11-28 00:22:45 +0100

    Updated lab4

--- lab4.m
+++ lab4.m
@@ -1,9 +1,11 @@
-% Note: Written using Octave
+% Note: This script was written in Octave.
+% There might be small differences when running it in Matlab
 
 % Exercise 1 - Noise
 
 clear; clc;
 
+% If using Matlab, move this function to a separate file
 function [] = func (s, N, bins, fs, title1, title2, title3, title4, fig)
   figure(fig);
   
@@ -15,7 +17,8 @@ function [] = func (s, N, bins, fs, title1, title2, title3, title4, fig)
   hist(s);
   title(title2);
   
-  [h, xh] = hist(s,bins,1); % 50 bins normalized histogram => pdf (sum(h)=1)
+  [h, xh] = hist(s,bins); % 50 bins histogram
+  h = h/N; % normalized histogram => pdf (sum(h)=1)
   subplot(2,2,3);
   plot(xh,h,'-*');
   title(title3);
@@ -26,14 +29,14 @@ function [] = func (s, N, bins, fs, title1, title2, title3, title4, fig)
   n = -fs/2 : step : fs/2 - step;
   mag = abs(dft);
   subplot(2,2,4);
-  stem(n, mag);
+  plot(n, mag);
   title(title4);
-endfunction
+end
 
 
 % 1.1, 1.2, 1.3
 fig = 1;
-N = 10**5;
+N = 10^5;
 fs = 10000; % sampling frequency = 10 kHz
 bins = 50;
 title1 = sprintf('s[n] (Gaussian noise) N=%d', N);
@@ -54,9 +57,8 @@ title2 = 'Histogram';
 title3 = 'PDF (hsitogram)';
 title4 = 'DFT - Magnitude';
 func (sb, N, bins, fs, title1, title2, title3, title4, fig);
-% The length of the signal is half of the original
-% The histogram has half the counts but the pdf curve is same
-% The length of the DFT is the same (10000 frequencies)
+% The pdf is similar
+% The frequency distribution is different
 
 % 1.5
 fig = 3;
@@ -67,35 +69,31 @@ title2 = 'Histogram';
 title3 = 'PDF (hsitogram)';
 title4 = 'DFT - Magnitude';
 func (sc, N, bins, fs, title1, title2, title3, title4, fig);
-% Frequency distribution is unchanged
-% the spatial distribution pdf is modified
+% The pdf is different
+% The frequency distribution is same
 
 
 % 1.6
+fig = 4;
 m = [1 1;2 2] / 2;
 sd = conv2(s,m,'same');
-Nd = length(sd);
 K = max(sd) * max(s);
-sd = sd / K;
-[h3, xh3] = hist(sd,50);
-h3 = h3/length(sd);
-figure();
-plot(xh3,h3,'*-');
-title('pdf - filtering 0.5*[1 1;2 2]');
+sd = sd / K; % normalized signal
+N = length(sd);
+title1 = sprintf('s[d] = [1 1;2 2]/2 * s[n] N=%d', N);
+title2 = 'Histogram';
+title3 = 'PDF - filtering 0.5*[1 1;2 2]';
+title4 = 'DFT - Magnitude';
+func (sd, N, bins, fs, title1, title2, title3, title4, fig);
+% The pdf is same
+% The frequency distribution is different
 
-dft = fft(sd);
-f = fftshift(dft);
-df = fs/Nd;
-figure();
-plot(-fs/2:df:fs/2-df,abs(f));
-% Frequency distribution is modified (filtering)
-% the spatial distribution pdf is unchanged
-
-clear; clc; close all;
 % Exercise 2 - DFT
-% 2.1
 
-figure();
+clear; clc;
+
+% 2.1
+figure(5);
 f = 5;
 fs = 50;
 t = 0:1/fs:1-1/fs;
@@ -105,7 +103,7 @@ freq = (-N/2 : N/2 -1) * fs/N;
 xf = fftshift(fft(xn));
 subplot(221);
 plot(t,xn);
-title('Signal');
+title('Sine Signal');
 xlabel('Times (sec)');
 ylabel('Amplitude');
 subplot(222);
@@ -124,8 +122,8 @@ title('Imaginary');
 xlabel('Frequency');
 ylabel('Im(X(f))');
 
-figure();
 % 2.2
+figure(6);
 f = 5;
 fs = 50;
 t = 0:1/fs:1-1/fs;
@@ -135,7 +133,7 @@ freq = (-N/2 : N/2 -1) * fs/N;
 xf = fftshift(fft(xn));
 subplot(221);
 plot(t,xn);
-title('Signal');
+title('Cosine Signal');
 xlabel('Times (sec)');
 ylabel('Amplitude');
 subplot(222);
@@ -154,9 +152,12 @@ title('Imaginary');
 xlabel('Frequency');
 ylabel('Im(X(f))');
 
+% The magnitude of the DFT of sine and cosine signals are same.
+% The real parts are different.
+% The imaginary parts have similar shape but different amplitudes.
 
-figure();
 % 2.3
+figure(7);
 f = 5;
 fs = 50;
 t = 0:1/fs:1-1/fs;
@@ -184,5 +185,3 @@ plot(t,imag(xf));
 title('Imaginary');
 xlabel('Frequency');
 ylabel('Im(X(f))');
-
-
